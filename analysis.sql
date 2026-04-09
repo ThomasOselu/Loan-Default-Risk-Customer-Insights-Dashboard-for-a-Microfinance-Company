d44c10b3bcd6125cc214bfec2bd7f6e8ec2ecd2a
danieloselu3 <doselu3@gmail.com>
2026-04-09 11:10:36 +0300

    cleaned data

---
+++ analysis.sql
@@ -0,0 +1,15 @@
+CREATE TABLE loans (
+    Customer_ID TEXT,
+    Age INTEGER,
+    Gender TEXT,
+    Income INTEGER,
+    Employment_Status TEXT,
+    Loan_Amount INTEGER,
+    Loan_Type TEXT,
+    Credit_Score INTEGER,
+    Loan_Term_Months INTEGER,
+    Repayment_Status TEXT,
+    Region TEXT,
+    Income_Group TEXT,
+    Risk_Level TEXT
+);
